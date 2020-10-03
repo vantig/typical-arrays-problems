7a634798a93d372a75ddc6d93f56b8fc579a8a63
Vanya Gordeychik <vanya.gordejchik@mail.ru>
2020-10-03 15:23:05 +0300

    init: start array task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array)
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array)
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+
+        let sum = array.reduce((a, b) => a + b, 0);
+
+        let result = sum / array.length;
+        return result;
+    }
 }
